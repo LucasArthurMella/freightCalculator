1a4f91c8bf713a68f154d3f703d5978a2d517523
LucasArthurMella <lucasmella16@gmail.com>
2024-08-16 22:09:11 -0300

    chore: mais algumas mudanças de detalhes e build

--- apps/front/src/util/masks.ts
+++
@@ -1,8 +0,0 @@
-export const cepMask = (value: String | undefined) => {
-    if (!value) return ''
-    value = value.replace(/\D/g, "")
-    .replace(/^(\d{5})(\d{3})+?$/, "$1-$2")
-    .replace(/(-\d{3})(\d+?)/, '$1')    
-    console.log(value);
-    return value;
-}
